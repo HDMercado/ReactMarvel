f390cea6758046f245cb4573b38eb350ed0c4032
Hugo Mercado <hd.mercadom@gmail.com>
2020-10-13 17:15:23 -0300

    deploy

---
+++ build/precache-manifest.8e2635ede7066a0607be96d307bd973c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95943aa0e935de5390e18d458835d532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e477d5a1cfeddecc8fd0",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c18910861fe95ad40910",
+    "url": "/static/css/main.57e46216.chunk.css"
+  },
+  {
+    "revision": "e477d5a1cfeddecc8fd0",
+    "url": "/static/js/2.465f098c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.465f098c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18910861fe95ad40910",
+    "url": "/static/js/main.e8976d26.chunk.js"
+  },
+  {
+    "revision": "b48ce41003832953836d",
+    "url": "/static/js/runtime-main.a2f8ebb8.js"
+  },
+  {
+    "revision": "eca914fbea093c20a2128ed33e26bac1",
+    "url": "/static/media/hojas.eca914fb.png"
+  },
+  {
+    "revision": "034d624420b9b2ea7fec3132919673c2",
+    "url": "/static/media/logo.034d6244.png"
+  }
+]);
